e589ec0416fc6afc484e5ad91bdeb247a90145bc
Yetunde015 <yettybaby210@gmail.com>
2021-12-11 00:52:02 -0500

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -44,8 +44,8 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-        
-    print('The count of all expenses:' + str(len(myBudgetList)))
+
+    print('The count of all expenses: ' + str(len(myBudgetList)))
 
     for entry in myBudgetList:
         print(entry)
