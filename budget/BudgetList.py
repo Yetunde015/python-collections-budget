fe659c35a7c4f88f027f90edd3a3b7f4b842ee79
Yetunde015 <yettybaby210@gmail.com>
2021-12-10 23:31:50 -0500

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -32,9 +32,9 @@ class BudgetList():
     def __next__(self):
         
         try:
-            return next(self.iter_e)
+            return self.iter_e.__next__()
         except StopIteration as stop:
-            return next(self.iter_o)
+            return self.iter_o.__next__()
 
 
 
@@ -53,8 +53,7 @@ def main():
     labels = ['Expenses', 'Overages', 'Budget']
     values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
     #values =  [myBudgetList: sum_expenses, sum_overages, budget]
-    color = ['green', 'red', 'blue'] 
-    ax.bar(labels, values,color=color)
+    ax.bar(labels, values,color=['green', 'red', 'blue'] )
     
     ax.set_title('Your total expenses vs total budget')
     plt.show()
